307ba669dd96731b9edb963dec0075c35555f074
Matias Sivadon <sivadon.m@gmail.com>
2022-09-08 16:24:09 -0300

    Primera entrega del proyecto final

--- js/arrays.js
+++ js/arrays.js
@@ -1,5 +1,6 @@
 class Producto {
-    constructor(nombre, precio, talle) {
+    constructor(id,nombre, precio, talle) {
+        this.id = id
         this.nombre = nombre
         this.precio = parseInt(precio)
         this.talle = talle
@@ -9,17 +10,17 @@ class Producto {
 const productos = [];
 
 function listarInventario () {
-productos.push(new Producto ("Buzo de lanilla con capucha", 1000, "1"));
-productos.push(new Producto("Remera Le Utthe", 500, "S"));
-productos.push(new Producto("Sweater coral", 800, "Único"));
-productos.push(new Producto("Camisa de encaje", 1200, "1"));
-productos.push(new Producto("Top con volados", 400, "1"));
-productos.push(new Producto("Remera de fibrana estampada", 500, "Único"));
-productos.push(new Producto("Parka", 8000, "44"));
-productos.push(new Producto("Polera", 800, "Único"));
-productos.push(new Producto("Sweater corto", 1800, "Único"));
-productos.push(new Producto("Conjunto de plush bordo", 5500, "Único"));
-productos.push(new Producto("Conjunto de plush gris", 5500, "Único"));
+productos.push(new Producto ("1","Buzo de lanilla con capucha", 1000, "1"));
+productos.push(new Producto("2","Remera Le Utthe", 500, "S"));
+productos.push(new Producto("3","Sweater coral", 800, "Único"));
+productos.push(new Producto("4","Camisa de encaje", 1200, "1"));
+productos.push(new Producto("5","Top con volados", 400, "1"));
+productos.push(new Producto("6","Remera de fibrana estampada", 500, "Único"));
+productos.push(new Producto("7","Parka", 8000, "44"));
+productos.push(new Producto("8","Polera", 800, "Único"));
+productos.push(new Producto("9","Sweater corto", 1800, "Único"));
+productos.push(new Producto("10","Conjunto de plush bordo", 5500, "Único"));
+productos.push(new Producto("11","Conjunto de plush gris", 5500, "Único"));
 console.table(productos)
 }
 
@@ -28,9 +29,22 @@ debugger
 let confirmacion = confirm("¿Desea comprar un producto?")
     if (confirmacion === true) {
         listarInventario()
-        let indice = parseInt(prompt("Ingrese el número de índice del producto que usted desea"))
-        productos.splice(indice,1)
-        console.table(productos)
+        let id = prompt("Ingrese el número de ID del producto que usted desea")
+        let productoSeleccionado = productos.filter (producto => producto.id === id)
+        console.table(productoSeleccionado)
+
+        let nuevoProducto = true
+        while (nuevoProducto) {
+            nuevoProducto = confirm("¿Desea agregar un nuevo producto a su carrito?")
+            if (nuevoProducto == true) {
+                id = prompt("Ingrese el número de ID del producto que usted desea")
+                let nuevoProductoSeleccionado = productos.filter (producto => producto.id === id)
+                productoSeleccionado = productoSeleccionado.concat(nuevoProductoSeleccionado)
+                console.table(productoSeleccionado)
+            } else {
+                alert("Gracias por su compra!")
+            }
+        }
     } else {
         alert("Gracias por su visita, esperamos vuelva pronto!")
     }
